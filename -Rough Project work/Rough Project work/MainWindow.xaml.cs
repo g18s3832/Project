5c0ba1abd8b1b3c3d56aaf7f6263792b3e5669ff
g18s3832 <phumezosinovuyosoka@gmail.com>
2018-09-19 18:34:49 +0200

    Add files via upload

---
+++ -Rough Project work/Rough Project work/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Rough_Project_work
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public class GameObjects
+    {
+        private string GetShip(int ship)
+        {
+            string shipname = "";
+            switch(ship)
+            {
+                case 1:
+                    shipname += Directory.GetCurrentDirectory() +"/Ships/Ship 1.png";
+                    break;
+                case 2:
+                    shipname += Directory.GetCurrentDirectory() + "/Ships/Ship 2.png";
+                    break;
+                case 3:
+                    shipname += Directory.GetCurrentDirectory() + "/Ships/Ship 3.png";
+                    break;
+                case 4:
+                    shipname += Directory.GetCurrentDirectory() + "/Ships/Ship 4.png";
+                    break;
+            }
+            return shipname;
+        }
+        public string Ship(int code)
+        {
+            return GetShip(code);
+        }
+        private string GetEnemy(int enemy)
+        {
+            string enemytype = "";
+            switch(enemy)
+            {
+                case 1:
+                    enemytype += Directory.GetCurrentDirectory() + "/Enemy/Enemy1.png";
+                    break;
+                case 2:
+                    enemytype += Directory.GetCurrentDirectory() + "/Enemy/Enemy2.png";
+                    break;
+                case 3:
+                    enemytype += Directory.GetCurrentDirectory() + "/Enemy/Enemy3.png";
+                    break;
+            }
+            return enemytype;
+        }
+        public string Enemy(int code)
+        {
+            return GetEnemy(code);
+        }
+
+        
+    }
+    public partial class MainWindow : Window
+    {
+        
+        string imagename = "awesome.jpg";
+        string filepath = Directory.GetCurrentDirectory();
+        int Cyclecount = 0;
+        public MainWindow()
+        {
+            //mess
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Cyclecount += 1;
+            if (Cyclecount == 5)
+                Cyclecount = 1;
+            switch (Cyclecount)
+            {
+                case 1:
+                    imagename = "cool.jpg";
+                    break;
+                case 2:
+                    imagename = "awesome.jpg";
+                    break;
+                case 3:
+                    imagename = "pussy.jpg";
+                        break;
+                case 4:
+                    imagename = "dope.jpg";
+                    break;
+            }
+            string imagepath = filepath + @"\" + imagename;
+           // MessageBox.Show(filepath);
+            // Image myimage= new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this),))
+            this.Background = new ImageBrush(new BitmapImage(new Uri(imagepath)));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//new game
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            Main_Menu.Hide();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)//continue
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)//options
+
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)//quit game
+        {
+
+        }
+       
+    }
+}
---
+++ -Rough Project work/Rough Project work/Window1.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Media;
+using System.Windows.Threading;
+using System.Windows.Resources;
+
+
+namespace Rough_Project_work
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        List <Image> Ammo = new List <Image>();
+        List<Image> Enemy = new List<Image>();
+        string ammo = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()) + @"\Ammo\Blasters\Blaster1.png";
+        string enemy = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()) + @"\Enemy\Enemy1.png";
+        string ammoSound = Directory.GetParent(Directory.GetParent(Directory.GetCurrentDirectory().ToString()).ToString()) + @"\Sounds\Blasters\Blaster1.wav";
+        double shipVelX = 15;
+        DispatcherTimer theTimer = new DispatcherTimer();
+        DispatcherTimer enemyTimer = new DispatcherTimer();
+        public Window1()
+        {
+            InitializeComponent();
+            Play.Visibility = Visibility.Hidden;
+            Main_Menu_Button.Visibility = Visibility.Hidden;
+            enemyTimer.Interval = TimeSpan.FromSeconds(1.5);
+            enemyTimer.IsEnabled = true;
+            enemyTimer.Tick += new EventHandler(enemyTimer_Tick);
+            theTimer.Interval = TimeSpan.FromMilliseconds(70);
+            theTimer.IsEnabled = true;
+            theTimer.Tick += new EventHandler(TheTimer_Tick);   
+        }
+        private void enemyTimer_Tick(object sender, EventArgs e)
+        {
+            Enemy.Add(LoadEnemy(enemy)); 
+        }
+        private void BlasterSound()
+        {
+            //Uri SoundPath = new Uri(Directory.GetCurrentDirectory() +"\\"+ @"Sounds\Blaster1.wav");
+            //StreamResourceInfo strm = Application.GetResourceStream(SoundPath);
+            SoundPlayer sp = new SoundPlayer((Directory.GetCurrentDirectory() + @"\" + @"Sounds\Blasters\Blaster1.wav"));
+            sp.Play();
+        }
+        private void TheTimer_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < Ammo.Count; i++)
+            {
+                updateProjectile(Ammo[i]);
+
+            }
+            for (int j = 0; j < Enemy.Count; j++)
+            {
+                updateEnemy(Enemy[j]);
+            }
+        }
+
+        /*private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            
+        }*/
+        private void updateEnemy(Image ene)
+        {
+            double NextY = Canvas.GetTop(ene) + shipVelX;
+            if (NextY <= canvas.ActualHeight-ene.ActualHeight)
+            {
+                Canvas.SetTop(ene, NextY);
+            }
+            else
+            {
+                canvas.Children.Remove(ene);
+            }
+
+        }
+        private void updateProjectile(Image img)
+        {
+            double nextY = Canvas.GetTop(img) - shipVelX;
+            if (nextY <= canvas.ActualHeight + img.ActualHeight)
+            {
+                Canvas.SetTop(img, nextY);
+            }
+            else
+            {
+                canvas.Children.Remove(img);
+            }
+        }
+        //public void Credits() { }
+        private void Window_Keydown(object sender,KeyEventArgs e)
+        {
+            double NextY = 0;
+            double NextX = 0;
+            NextX = (Canvas.GetLeft(ship));
+            NextY = Canvas.GetTop(ship);
+            switch(e.Key)
+            {
+                case Key.Left:
+                    NextX -= shipVelX;
+                    if (NextX < 0 || NextX + ship.ActualWidth >= canvas.ActualWidth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Canvas.SetLeft(ship, NextX);
+                    }
+                    break;
+                case Key.Right:
+                    NextX += shipVelX;
+                    if (NextX < 0 || NextX + ship.ActualWidth >= canvas.ActualWidth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Canvas.SetLeft(ship, NextX);
+                    }
+                    break;
+
+                case Key.Down:
+                    NextY += shipVelX;
+                    if (NextY < 0 || NextY + ship.ActualHeight >= canvas.ActualHeight)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Canvas.SetTop(ship, NextY);
+                    }
+                    break;
+                case Key.Up:
+                    NextY -= shipVelX;
+                    if (NextY < 0 || NextY + ship.ActualHeight >= canvas.ActualHeight)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Canvas.SetTop(ship, NextY);
+                    }
+                    break;
+                case Key.Escape:
+                    Pause();
+                    break;
+                case Key.Space:
+                    Ammo.Add(LoadAmmo(ammo));
+                    BlasterSound();
+                    TheTimer_Tick(Ammo, e);
+                    break;
+            }
+        }
+        private void Pause()
+        {
+            theTimer.Stop();
+            Play.Visibility = Visibility.Visible;
+            Main_Menu_Button.Visibility = Visibility.Visible;
+        }
+        private Image LoadAmmo(string ammo)
+        {
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(ammo));
+            img.Height = (20);
+            img.Width = 10;
+            canvas.Children.Add(img);
+            Canvas.SetLeft(img, Canvas.GetLeft(ship) + (ship.ActualWidth / 2)-3);
+            Canvas.SetTop(img, Canvas.GetTop(ship));
+            return img;   
+        }
+        private Image LoadEnemy(string enmy)
+        {
+            Image enm = new Image();
+            enm.Source = new BitmapImage(new Uri(enmy));
+            enm.Height = 50;
+            enm.Width = 45;
+            Random pos = new Random();
+            canvas.Children.Add(enm);
+            Canvas.SetLeft(enm, pos.Next((int)enm.ActualWidth,((int)(canvas.ActualWidth)-(int)(enm.ActualWidth))));
+            Canvas.SetTop(enm, 0);
+            return enm;
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)//panel
+        {
+
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            theTimer.Start();
+            Play.Visibility = Visibility.Hidden;
+            Main_Menu_Button.Visibility = Visibility.Hidden;
+        }
+    }
+}
